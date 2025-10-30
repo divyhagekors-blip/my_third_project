1b119f3c9e587a150961343d364b7e1bd3ae5e8c
divyhagekors-blip <divyhagekors@gmail.com>
2025-10-30 17:39:29 +0530

    vfdvhbj

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_demo/screen/Homescreen.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.blue
-        ),
-        home: Homescreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue
       ),
+      home: Homescreen(),
     );
   }
 }
