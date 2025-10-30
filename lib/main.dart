91322c90d6b7a36ebdab78d71ce58b69f2dd546a
divyhagekors-blip <divyhagekors@gmail.com>
2025-10-30 15:27:11 +0530

    main.dart add in the github

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_demo/screen/Homescreen.dart';
+
+void main(){
+  runApp(ProviderScope(child: MyApp()));
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: Homescreen(),
+    );
+  }
+}
