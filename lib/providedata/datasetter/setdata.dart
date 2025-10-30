0508da257cec1b09115e638951deec9a154d9200
divyhagekors-blip <divyhagekors@gmail.com>
2025-10-30 12:19:01 +0530

    add scaffold in rhfvjh

--- lib/providedata/datasetter/setdata.dart
+++ lib/providedata/datasetter/setdata.dart
@@ -10,6 +10,8 @@ class Setdata extends StatefulWidget {
 class _SetdataState extends State<Setdata> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("divy"),),
+    );
   }
 }
