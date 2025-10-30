12dc7542ed0010ab7772d5f03b78acf5a20ddac9
divyhagekors-blip <divyhagekors@gmail.com>
2025-10-30 12:05:58 +0530

    make new directory

---
+++ lib/providedata/datasetter/setdata.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Setdata extends StatefulWidget {
+  const Setdata({super.key});
+
+  @override
+  State<Setdata> createState() => _SetdataState();
+}
+
+class _SetdataState extends State<Setdata> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
